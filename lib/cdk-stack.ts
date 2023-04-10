7bcb3a10c527d75cbfab72c3466ce82ea068ec70
Keynzu <keynerolivo@gmail.com>
2023-04-09 21:09:52 -0500

    Template created and App deployed

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,10 +1,28 @@
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CloudFrontToS3 } from '@aws-solutions-constructs/aws-cloudfront-s3' 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const imageCloudfront = new CloudFrontToS3(this, 'image-cloudfront', {
+      insertHttpSecurityHeaders: false,
+      bucketProps: {
+        bucketName: 'image-inventory-bucket',
+        removalPolicy: RemovalPolicy.DESTROY,
+        autoDeleteObjects: true
+      },
+      cloudFrontLoggingBucketProps: {
+        removalPolicy: RemovalPolicy.DESTROY,
+        autoDeleteObjects: true,
+      },
+      loggingBucketProps: {
+        removalPolicy: RemovalPolicy.DESTROY,
+        autoDeleteObjects: true,
+      }
+    })
 
     // The code that defines your stack goes here
 
